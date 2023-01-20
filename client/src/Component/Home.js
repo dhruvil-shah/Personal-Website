77c89c284ba8a41648a24c15e1427645059c2a70
dhruvil-shah <67452891+dhruvil-shah@users.noreply.github.com>
2023-01-20 20:01:27 +0530

    Update Home.js

--- client/src/Component/Home.js
+++ client/src/Component/Home.js
@@ -9,7 +9,7 @@ export const Home = () => {
             <Zoom left>
             <h1>Hey Dhruvil Here!!</h1>
             </Zoom>
-            <img src='https://media-exp1.licdn.com/dms/image/C4E03AQEEcvbtDvdO0w/profile-displayphoto-shrink_800_800/0/1607868193277?e=1637798400&v=beta&t=gLUgOUaTOyKSCniMpexMLIjeI0QUao7eNec-fifXq_8'/>
+//             <img src='https://media-exp1.licdn.com/dms/image/C4E03AQEEcvbtDvdO0w/profile-displayphoto-shrink_800_800/0/1607868193277?e=1637798400&v=beta&t=gLUgOUaTOyKSCniMpexMLIjeI0QUao7eNec-fifXq_8'/>
             <Slide left>
             <p>I'm currently studying as a 3rd year Computer Science student at Nirma University, Ahmedabad. I have been in recent times learned ample number of skills such as 
 Web Development, Machine Learning, Databases, Full Stack Development, Application Development and much more. I have also interest in problem solving on various 
